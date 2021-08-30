142edaef0680830855e7c1b8a5d264aaf8f9edab
steveplayshorn <steveplayshorn@gmail.com>
2021-08-29 23:22:10 -0500

    Started work on new channels.

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,9 +1,18 @@
 import sidebarStyles from '../styles/Sidebar.module.css'
 
-const Sidebar = () => {
+const Sidebar = ({setChannel}) => {
+    const handleClick = (e) => {
+        console.log(e.target.innerHTML)
+        setChannel(e.target.innerHTML)
+    }
     return (
         <div className={sidebarStyles.sidebar}>
-            SIDEBAR!!!!
+            <ul>
+                <li onClick={handleClick}>Channel 1</li>
+                <li onClick={handleClick}>Channel 2</li>
+                <li onClick={handleClick}>Channel 3</li>
+                <li onClick={handleClick}>Channel 4</li>
+            </ul>
         </div>
     )
 }

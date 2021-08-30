9ab43e1222d248d5d5edda882343a2730974187c
steveplayshorn <steveplayshorn@gmail.com>
2021-08-29 22:56:11 -0500

    Achieved chat functionality.

--- client/seed.js
+++ client/seed.js
@@ -1,60 +1,70 @@
 const chatData = [
     {
-        "name": "Steven",
+        "user": "Steven",
+        "room": "main",
         "body": "This is a message. It's my first one, omg!!!",
         "date": "8/25/2021",
         "time": "19:13:39"
     },
     {
-        "name": "Steven",
+        "user": "Steven",
+        "room": "main",
         "body": "This is a message. It's my second one, omg!!!",
         "date": "8/25/2021",
         "time": "19:13:39"
     },
     {
-        "name": "Steven",
+        "user": "Steven",
+        "room": "main",
         "body": "This is a message. It's my third one, omg!!!",
         "date": "8/25/2021",
         "time": "19:13:39"
     },
     {
-        "name": "Steven",
+        "user": "Steven",
+        "room": "main",
         "body": "This is a message. It's my forth one, omg!!!",
         "date": "8/25/2021",
         "time": "19:13:39"
     },
     {
-        "name": "Steven",
+        "user": "Steven",
+        "room": "main",
         "body": "This is a message. It's my fifth one, omg!!!",
         "date": "8/25/2021",
         "time": "19:13:39"
     },
     {
-        "name": "Steven",
+        "user": "Steven",
+        "room": "main",
         "body": "This is a message. It's my sixth one, omg!!!",
         "date": "8/26/2021",
         "time": "19:13:39"
     },
     {
-        "name": "Steven",
+        "user": "Steven",
+        "room": "main",
         "body": "This is a message. It's my seventh one, omg!!!",
         "date": "8/26/2021",
         "time": "19:13:39"
     },
     {
-        "name": "Steven",
+        "user": "Steven",
+        "room": "main",
         "body": "This is a message. It's my eighth one, omg!!!",
         "date": "8/26/2021",
         "time": "19:13:39"
     },
     {
-        "name": "Steven",
+        "user": "Steven",
+        "room": "main",
         "body": "This is a message. It's my ninth one, omg!!!",
         "date": "8/26/2021",
         "time": "19:13:39"
     },
     {
-        "name": "Steven",
+        "user": "Steven",
+        "room": "main",
         "body": "This is a message. It's my tenth one, omg!!!",
         "date": "8/26/2021",
         "time": "19:13:39"

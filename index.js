ce5ca27b24b21acd10da0c5ac7c0ed6412ef7ca0
steveplayshorn <steveplayshorn@gmail.com>
2021-09-17 18:14:10 -0500

    Updated urls.

--- index.js
+++ index.js
@@ -76,7 +76,8 @@ app.post('/messages', async(req, res) => {
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        // origin: "http://localhost:3000",
+        origin: "https://zoom-slack.vercel.app",
         methods: ['GET', 'POST'],
     },
 })

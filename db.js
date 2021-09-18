6966157d4a43b56b087510c80636f54584eaa3f4
steveplayshorn <steveplayshorn@gmail.com>
2021-09-17 19:10:27 -0500

    deploy

--- db.js
+++ db.js
@@ -15,6 +15,8 @@ const proConfig = {
 
 const pool = new Pool(process.env.NODE_ENV === 'production' ? proConfig : devConfig)
 pool.connect((err, client, done) => {
+    console.log(process.env.NODE_ENV)
+    console.log(process.env.DATABASE_URL)
     if (err) {
         console.log('HI ERROR!!!!')
         console.log(err)
@@ -24,3 +26,5 @@ pool.connect((err, client, done) => {
 })
 
 module.exports = pool
+
+postgres://jnjegafsefclsr:5c8193ba6c9e7651d7e2c2ffd0efb306027652d1446a34d6c29f15cd9e9e4e7e@ec2-54-158-247-97.compute-1.amazonaws.com:5432/d8vcq1crfrtkvm

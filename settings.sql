f635cf2b221c92bf71beacf5aa24968ac7ec0422
Steven Winter <stevenwinter.me@gmail.com>
2025-12-13 20:23:58 -0500

    Added DbGate tables.

--- settings.sql
+++ settings.sql
@@ -21,3 +21,28 @@ CREATE TABLE message(
 );
 
 INSERT INTO users (user_name, user_email, user_password, user_avatar) VALUES ('Steven', 'test@gmail.com', 'test123', 'https://upload.wikimedia.org/wikipedia/en/0/0f/Charlie_Kelly.png');
+
+-- DbGate Tables
+
+-- CREATE TABLE "public"."users" ( 
+--   "user_id" SERIAL,
+--   "user_name" TEXT NOT NULL,
+--   "user_email" TEXT NOT NULL,
+--   "user_password" TEXT NOT NULL,
+--   "user_avatar" TEXT NULL,
+--   "id" INTEGER NOT NULL,
+--   CONSTRAINT "PK_users" PRIMARY KEY ("user_id")
+-- );
+
+-- CREATE TABLE "public"."message" ( 
+--   "message_id" SERIAL,
+--   "user_id" INTEGER NULL,
+--   "user_name" TEXT NULL,
+--   "user_avatar" TEXT NULL,
+--   "body" TEXT NULL,
+--   "channel" TEXT NULL,
+--   "date" TEXT NULL,
+--   "time" TIME WITHOUT TIME ZONE NULL,
+--   CONSTRAINT "PK_message" PRIMARY KEY ("message_id"),
+--   CONSTRAINT "fk_user" FOREIGN KEY ("user_id") REFERENCES "public"."users" ("user_id")
+-- );
